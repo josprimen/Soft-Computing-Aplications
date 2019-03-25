4b217871a00136935a33d8678282ad64eb5b4ac3
josprimen <36623141+josprimen@users.noreply.github.com>
2019-03-25 18:16:51 +0100

    Minor changes in zdt3.py implementation

--- zdt3.py
+++ zdt3.py
@@ -7,20 +7,17 @@ class zdt3:
     def __init__(self):
         self.min_realvar = [0.0 for _ in range(30)]
         self.max_realvar = [1.0 for _ in range(30)]
-        self.nreal = 30
-        self.nbin = 0
-        self.con = 0
-        self.nobj = 2
+        self.obj = []
 
 # Creamos la función para obtención de resultados
 
-    def solucion(self, xreal, xbin, gene, obj, constr):
+    def solucion(self, xreal):
         n=30
         tmp = 0
-        obj[0] = xreal[0]
+        self.obj[0] = xreal[0]
         for i in range(1,n):
             tmp = tmp + xreal[i]
         g = 1 + (9/n-1) * tmp
         h = 1 - math.sqrt(xreal[0]/g) - (xreal[0]/g)*math.sin(10*math.pi*xreal[0])
-        obj[2] = g*h
-        return obj
+        self.obj[1] = g*h
+        return self.obj
